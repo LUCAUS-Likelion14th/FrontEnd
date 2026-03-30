75cead63dfc7eff5bba0ab3623717133638d46b1
eunjilee0311 <leg040311@gmail.com>
2026-03-30 17:32:26 +0900

    feat: 아티스트 버튼 UI 구현

---
+++ components/stage/ArtistButton.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+
+type ArtistButtonProps = {
+  image: string;
+  artist: string;
+  onClick?: () => void;
+};
+
+export default function ArtistButton({
+  image,
+  artist,
+  onClick,
+}: ArtistButtonProps) {
+  return (
+    <div className="flex gap-4">
+      <div className="flex flex-col items-center gap-1">
+        <button
+          onClick={onClick}
+          className="w-19.5 h-19.5 rounded-full overflow-hidden"
+        >
+          <Image
+            src={image}
+            alt={`${artist} 사진`}
+            width={78}
+            height={78}
+            className="object-cover"
+          />
+        </button>
+        <p className="text-[14px]">{artist}</p>
+      </div>
+    </div>
+  );
+}
