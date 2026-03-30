703dd6f8fc83636b8f9aa1a1c2e994761ba147d6
Jihaeee <elinkang02@gmail.com>
2026-03-30 14:28:44 +0900

    feat: 홈 화면 상단 공지 이미지 캐러셀 구현 (#2)

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 import NoticeBanner from "@/components/home/NoticeBanner";
 import StampShortcutButton from "@/components/home/StampShortcutButton";
+import ImageSwiper from "@/components/ui/ImageSwiper";
 
 export default function Home() {
   return (
     <main className="p-4">
-      <div className="flex flex-col gap-2">
-        <NoticeBanner />
-        <StampShortcutButton />
+      <div className="flex flex-col gap-8">
+        <ImageSwiper />
+        <div className="flex flex-col gap-2">
+          <NoticeBanner />
+          <StampShortcutButton />
+        </div>
       </div>
     </main>
   );
