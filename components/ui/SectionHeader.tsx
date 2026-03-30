6de316aa5d91e58defb1eef02df16815dbcd8d6e
Jihaeee <elinkang02@gmail.com>
2026-03-30 16:58:32 +0900

    feat: 공연 카드 컴포넌트 구현 (#3)

---
+++ components/ui/SectionHeader.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import { FiChevronRight } from "react-icons/fi";
+
+type SectionHeaderProps = {
+  icon: string;
+  title: string;
+  description: string;
+  href: string;
+};
+
+export default function SectionHeader({ icon, title, description, href }: SectionHeaderProps) {
+  return (
+    <Link href={href} className="flex flex-col gap-[2px]">
+      <div className="flex items-center justify-between">
+        <h2 className="font-bold text-2xl">{icon} {title}</h2>
+        <FiChevronRight size={20} className="text-[#727272]" />
+      </div>
+      <p className="text-base text-text-sub">{description}</p>
+    </Link>
+  );
+}
