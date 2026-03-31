0484daa0a14b05bcde5f9633ecf7aeb0c85d4f38
eunjilee0311 <leg040311@gmail.com>
2026-03-31 16:21:24 +0900

    feat: 타임라인 UI 구현

--- components/stage/StageTimeline.tsx
+++ components/stage/StageTimeline.tsx
@@ -1,22 +1,63 @@
-import StageCard from "./StageCard";
+import TimelineCard from "./TimelineCard";
 
 type Props = {
   data: {
     id: number;
-    image: string;
+    stageImage: string;
     description: string;
     artist: string;
+    start: string;
+    end: string;
   }[];
+  activeId?: number;
 };
 
-export default function StageTimeline({ data }: Props) {
+export default function StageTimeline({ data, activeId }: Props) {
+  const formatTime = (time: string) => {
+    const date = new Date(time);
+    const hours = date.getHours().toString().padStart(2, "0");
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    return `${hours}:${minutes}`;
+  };
+
   return (
-    <div className="flex justify-between border rounded-[10px] px-3 py-3.5">
-      <section>타임라인</section>
+    <div className="flex justify-between border border-text-sub rounded-[10px] px-3 py-3.5">
+      <section className="flex flex-col items-start">
+        {data.map((item) => (
+          <div key={item.id} className="flex items-start mt-1.75 gap-3">
+            <div
+              className={`text-[14px] leading-3.5 ${
+                item.id === activeId
+                  ? "text-[#05F] font-semibold"
+                  : "text-text-sub"
+              }`}
+            >
+              {formatTime(item.start)} - {formatTime(item.end)}
+            </div>
+
+            <div className="flex flex-col items-center mt-0.75 gap-2.5">
+              <div
+                className={`rounded-full ${
+                  item.id === activeId
+                    ? "w-3 h-3 bg-[#05F] border-2"
+                    : "w-2 h-2 bg-white border border-text-sub"
+                }`}
+              />
+              <div className="h-21.75 w-px bg-text-sub" />
+            </div>
+          </div>
+        ))}
+      </section>
 
       <section className="flex flex-col">
         {data.map((item) => (
-          <StageCard key={item.id} {...item} />
+          <TimelineCard
+            key={item.id}
+            image={item.stageImage}
+            description={item.description}
+            artist={item.artist}
+            isActive={item.id === activeId}
+          />
         ))}
       </section>
     </div>
