f902871f5ebcbead6f99617d1e92dfd21f769882
eunjilee0311 <leg040311@gmail.com>
2026-04-01 16:23:58 +0900

    refactor: 코드 구조 개선

--- components/stage/ArtistButton.tsx
+++ components/stage/ArtistButton.tsx
@@ -9,19 +9,20 @@ type ArtistButtonProps = {
 
 export default function ArtistButton({ id, image, artist }: ArtistButtonProps) {
   return (
-    <Link href={`/stage/${id}`}>
-      <div className="flex flex-col items-center gap-1 cursor-pointer">
-        <div className="w-19.5 h-19.5 rounded-full overflow-hidden [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-          <Image
-            src={image}
-            alt={`${artist} 사진`}
-            width={78}
-            height={78}
-            className="object-cover"
-          />
-        </div>
-        <p className="text-[14px]">{artist}</p>
+    <Link
+      href={`/stage/${id}`}
+      className="flex flex-col items-center gap-1 cursor-pointer"
+    >
+      <div className="w-19.5 h-19.5 rounded-full overflow-hidden">
+        <Image
+          src={image}
+          alt={`${artist} 사진`}
+          width={78}
+          height={78}
+          className="object-cover"
+        />
       </div>
+      <p className="text-[14px]">{artist}</p>
     </Link>
   );
 }
