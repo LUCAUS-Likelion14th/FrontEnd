494cb3adbf52816a9ad05730c78353e4a9900a54
Jihaeee <elinkang02@gmail.com>
2026-04-06 01:18:40 +0900

    feat: 마이페이지 로그인 상태일 때 UI 구현 (#15)

--- app/mypage/page.tsx
+++ app/mypage/page.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation"
 import Image from "next/image"
 import { MyPageData } from "@/types/mypage"
+import { FiChevronRight } from "react-icons/fi"
 
 const mockData: MyPageData = {
   name: "푸앙이",
@@ -23,7 +24,7 @@ const TOTAL_STAMPS = 8 // TODO: 실제 전체 스탬프 개수로 변경
 
 export default function MyPage() {
   const router = useRouter()
-  const isLoggedIn = false // TODO: 실제 인증 상태로 변경
+  const isLoggedIn = true // TODO: 실제 인증 상태로 변경
   const data = mockData
 
   return (
@@ -32,7 +33,8 @@ export default function MyPage() {
 
       {!isLoggedIn ? (
         <>
-          <div className="flex items-center justify-between bg-[#C0C0C0] pt-17.5 pb-4 px-4">
+        {/* 비로그인 상태일 때 */}
+          <div className="flex items-end justify-between bg-[#C0C0C0] pt-17.5 pb-4 px-7.5">
             <div className="flex flex-col text-base font-semibold text-white">
               <span>로그인 후</span>
               <span >청:ON을 더 즐겨보세요!</span>
@@ -59,7 +61,69 @@ export default function MyPage() {
         </>
       ) : (
         <>
-          
+          {/* 로그인 상태일 때 */}
+            <div className="flex items-end justify-between bg-[#C0C0C0] pt-17.5 pb-4 px-7.5">
+                <span className="text-base font-semibold text-white">{data.name}님 환영합니다!</span>
+                <button className="bg-white text-primary text-base font-semibold w-25 rounded-[28px] p-2.5">
+                    로그아웃
+                </button>
+            </div>
+
+            <div className="flex flex-col p-4 gap-5">
+                
+            {/* 내 좋아요 */}
+            <div className="flex flex-col gap-5 bg-white border border-text-sub2 rounded-[10px] p-4">
+                <div className="flex items-center justify-between">
+                    <span className="text-2xl font-semibold">내 좋아요</span>
+                    <button onClick={() => router.push("/mypage/likes")}>
+                        <FiChevronRight size={24} className="text-[#727272]" />
+                    </button>
+                </div>
+                <div className="grid grid-cols-3 gap-2">
+                {data.booth_like_list.map((booth) => (
+                    <div key={booth.booth_id} className="flex flex-col border border-gray-200 rounded-[7px]">
+                    <div className="relative aspect-square">
+                        <Image src={booth.booth_image} alt={booth.booth_name} fill className="object-cover" />
+                    </div>
+                    <div className="px-2 py-1 border-light-gray bg-gradient-to-t from-white to-[#d9d9d9]">
+                        <span className="text-sm">{booth.booth_name}</span>
+                    </div>
+                    </div>
+                ))}
+                {data.food_like_list.map((truck) => (
+                    <div key={truck.food_id} className="flex flex-col border border-gray-200 rounded-[10px] overflow-hidden">
+                    <div className="relative aspect-square">
+                        <Image src={truck.image} alt={truck.food_name} fill className="object-cover" />
+                    </div>
+                    <div className="px-2 py-1 border-light-gray bg-gradient-to-t from-white to-[#d9d9d9]">
+                        <span className="text-sm">{truck.food_name}</span>
+                    </div>
+                    </div>
+                ))}
+                </div>
+                
+            </div>
+
+            {/* 도장판 */}
+            <div className="flex flex-col gap-4 bg-white border border-text-sub2 rounded-[10px] p-4">
+                <div className="flex items-center justify-between mb-5">
+                <span className="text-2xl font-semibold">도장판</span>
+                <button onClick={() => router.push("/mypage/stamp")}>
+                <FiChevronRight size={24} className="text-[#727272]" />
+                </button>
+                </div>
+                <span className="text-base text-center">
+                {TOTAL_STAMPS}개 중 {data.stamp_count}개를 모았어요
+                </span>
+                <div className="w-full bg-gray-200 rounded-full h-2">
+                <div
+                    className="bg-primary h-2 rounded-full"
+                    style={{ width: `${(data.stamp_count / TOTAL_STAMPS) * 100}%` }}
+                />
+                </div>
+                <span className="text-base text-text-sub text-center">광장기획전에 참여하고 푸짐한 경품 받아가세요!</span>
+            </div>
+            </div>
         </>
       )}
     </main>
