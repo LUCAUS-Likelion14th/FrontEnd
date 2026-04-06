8a4208fc65056ceea57e036d676bf8c7fbc1c080
eunjilee0311 <leg040311@gmail.com>
2026-04-06 13:53:16 +0900

    refactor: id 타입 string -> number 변경

--- data/notice.ts
+++ data/notice.ts
@@ -1,25 +1,33 @@
 type Category = "important" | "notice" | "event";
 
 interface NoticeProps {
+  id: number;
   category: Category;
   title: string;
   date: string;
+  content: string;
 }
 
 export const notices: NoticeProps[] = [
   {
+    id: 1,
     category: "important",
     title: "축제 일정 안내입니다",
     date: "04/01 12:00",
+    content: "내용",
   },
   {
+    id: 2,
     category: "notice",
     title: "일반 공지입니다",
     date: "04/02 13:00",
+    content: "내용",
   },
   {
+    id: 3,
     category: "event",
     title: "이벤트 참여하세요!",
     date: "04/03 14:00",
+    content: "내용",
   },
 ];
