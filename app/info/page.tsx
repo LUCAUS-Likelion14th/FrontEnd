8675083a13d51aa1a5f2d8cd3f0ec1fc685db9e2
eunjilee0311 <leg040311@gmail.com>
2026-04-06 10:55:18 +0900

    feat: 각 페이지 link 추가

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -6,11 +6,19 @@ export default function InfoPage() {
       <h1 className="text-[24px] font-semibold mb-5">안내</h1>
 
       <section className="flex flex-col gap-3">
-        <InfoCard imageUrl="/img.png" title="축제기획단 공지" link="" />
-        <InfoCard imageUrl="/img.png" title="분실물 안내" link="" />
-        <InfoCard imageUrl="/img.png" title="배리어프리" link="" />
-        <InfoCard imageUrl="/img.png" title="통행 정책" link="" />
-        <InfoCard imageUrl="/img.png" title="크레딧" link="" />
+        <InfoCard
+          imageUrl="/img.png"
+          title="축제기획단 공지"
+          link="info/notice"
+        />
+        <InfoCard imageUrl="/img.png" title="분실물 안내" link="info/lost" />
+        <InfoCard
+          imageUrl="/img.png"
+          title="배리어프리"
+          link="info/barrier-free"
+        />
+        <InfoCard imageUrl="/img.png" title="통행 정책" link="info/route" />
+        <InfoCard imageUrl="/img.png" title="크레딧" link="info/credit" />
       </section>
     </main>
   );
