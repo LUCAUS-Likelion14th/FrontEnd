82e9ae844e7bcc7a12fcaf11bcd6d199b21dbf0f
eunjilee0311 <leg040311@gmail.com>
2026-04-06 13:54:55 +0900

    refactor: 뒤로가기 버튼 컴포넌트 사용 및 id props 추가

--- app/info/notice/page.tsx
+++ app/info/notice/page.tsx
@@ -1,19 +1,20 @@
+import BackButton from "@/components/common/BackButton";
 import NoticeItem from "@/components/info/NoticeItem";
 import { notices } from "@/data/notice";
-import { FiChevronLeft } from "react-icons/fi";
 
 export default function NoticePage() {
   return (
     <main className="px-4 pt-2.5 pb-25">
       <div className="flex items-center gap-1 mb-4">
-        <FiChevronLeft size={24} className="cursor-pointer" />
+        <BackButton />
         <h1 className="text-[24px] font-semibold">축제기획단 공지</h1>
       </div>
 
       <section className="border-t border-[#DCE2E9]">
-        {notices.map((notice, idx) => (
+        {notices.map((notice) => (
           <NoticeItem
-            key={idx}
+            key={notice.id}
+            id={notice.id}
             category={notice.category}
             title={notice.title}
             date={notice.date}
