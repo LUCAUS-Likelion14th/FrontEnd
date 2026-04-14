11697145042d8641b5727d7650a3db487d94f6fb
eunjilee0311 <leg040311@gmail.com>
2026-04-14 13:20:22 +0900

    design: 로그인페이지 디자인 적용

--- components/common/GoogleLoginButton.tsx
+++ components/common/GoogleLoginButton.tsx
@@ -1,18 +1,20 @@
-"use client"
+"use client";
 
-import Image from "next/image"
-import { useRouter } from "next/navigation"
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function GoogleLoginButton() {
-  const router = useRouter()
+  const router = useRouter();
 
   return (
     <button
       onClick={() => router.push("/login")}
-      className="w-full flex items-center justify-center border border-gray-200 rounded-[10px] shadow-[0_2px_3px_0_rgba(0,0,0,0.168),0_0_3px_0_rgba(0,0,0,0.084)]"
+      className="w-full flex items-center justify-center bg-white/20 rounded-[10px] gap-5"
     >
-      <Image src="/icons/google.png" alt="구글" width={24} height={24} />
-      <span className="text-xl font-medium text-[#0000008A] p-[15px]">Google로 시작하기</span>
+      <Image src="/icons/google-logo.png" alt="구글" width={24} height={24} />
+      <span className="text-[20px] font-medium text-white py-3.5">
+        Google로 시작하기
+      </span>
     </button>
-  )
+  );
 }
