8ca5a09938398c1f29ea67e40608faed3e5c2486
eunjilee0311 <leg040311@gmail.com>
2026-04-16 10:35:01 +0900

    refactor: 좋아요 버튼 분리 및 효과 적용

--- components/ui/ListCard.tsx
+++ components/ui/ListCard.tsx
@@ -2,22 +2,31 @@ import Image from "next/image";
 import Link from "next/link";
 import { FiHeart } from "react-icons/fi";
 import { FaHeart } from "react-icons/fa";
+import LikeButton from "../common/LikeButton";
 
 type ListCardProps = {
   imageUrl: string;
   href: string;
   location: string;
   name: string;
-  likes: number;
-  liked: boolean;
+  isLiked: boolean;
+  likeCount: number;
+
   department?: string;
 };
 
-export default function ListCard({ imageUrl, href, location, name, likes, liked, department }: ListCardProps) {
+export default function ListCard({
+  imageUrl,
+  href,
+  location,
+  name,
+  isLiked,
+  likeCount,
+  department,
+}: ListCardProps) {
   return (
     <Link href={href} className="ml-[30px]">
       <article className="flex items-center gap-[13px] w-full p-2.5 rounded-[10px] bg-white border border-text-sub2">
-
         <Image
           src={imageUrl}
           alt={name}
@@ -33,17 +42,14 @@ export default function ListCard({ imageUrl, href, location, name, likes, liked,
           </div>
 
           <div className="flex flex-col items-end gap-3 shrink-0">
-            <div className="flex items-center gap-1">
-              <span className="text-base text-text-sub">{likes}</span>
-              {liked
-                ? <FaHeart size={16} className="text-primary" />
-                : <FiHeart size={16} className="text-text-sub" />
-              }
-            </div>
+            <LikeButton
+              initialIsLiked={isLiked}
+              initialLikeCount={likeCount}
+              layout="horizontal"
+            />
             <span className="text-base text-text-sub">{department}</span>
           </div>
         </div>
-
       </article>
     </Link>
   );
