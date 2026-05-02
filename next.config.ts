839ad7fa0df45729fcfab18e44b6f170ac3eb682
eunjilee0311 <leg040311@gmail.com>
2026-05-02 16:33:18 +0900

    fix: next/image 외부 S3 도메인 허용 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "lucaus-994851303288-ap-northeast-2-an.s3.ap-northeast-2.amazonaws.com",
+    ],
+  },
 };
 
 export default nextConfig;
