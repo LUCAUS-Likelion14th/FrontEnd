2a39a1d0e38cb86c571f6ba23a259ee933e4d1e8
Jihaeee <elinkang02@gmail.com>
2026-05-04 11:00:47 +0900

    fix: 빌드 오류 수정

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,25 @@
 import type { NextConfig } from "next";
 
+const API_URL =
+  process.env.API_URL ??
+  process.env.NEXT_PUBLIC_API_URL ??
+  "https://lucaus.o-r.kr";
+
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      "lucaus-994851303288-ap-northeast-2-an.s3.ap-northeast-2.amazonaws.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname:
+          "lucaus-994851303288-ap-northeast-2-an.s3.ap-northeast-2.amazonaws.com",
+      },
     ],
   },
   async rewrites() {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.API_URL}/:path*`,
+        destination: `${API_URL}/:path*`,
       },
     ];
   },
