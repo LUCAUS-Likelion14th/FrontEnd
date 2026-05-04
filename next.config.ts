6d59dff10ae4211fbfbb11d03cb31be83bdaea52
Jihaeee <elinkang02@gmail.com>
2026-05-04 10:38:59 +0900

    fix: cors에러 해결을 위해 프록시 설정

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,14 @@ const nextConfig: NextConfig = {
       "lucaus-994851303288-ap-northeast-2-an.s3.ap-northeast-2.amazonaws.com",
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: `${process.env.API_URL}/:path*`,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
