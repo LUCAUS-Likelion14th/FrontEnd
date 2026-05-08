71f1f19b2a0d0d9d0ac1771956a778377a2423ec
Jihaeee <elinkang02@gmail.com>
2026-05-08 23:09:12 +0900

    refactor: BottomNav 하나로 합치고 LoginBottomNav 삭제

--- src/assets/webp/index.ts
+++ src/assets/webp/index.ts
@@ -1,3 +1,5 @@
 import noticeIcon from './notice-icon.webp';
+import loginNavHome from './login-nav-home.webp';
+import loginNavInfo from './login-nav-info.webp';
 
-export { noticeIcon } ;
+export { noticeIcon, loginNavHome, loginNavInfo };
