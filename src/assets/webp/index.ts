76f33466b97d05e45ba38b9fcb581f81bebb03f1
Jihaeee <elinkang02@gmail.com>
2026-05-08 23:11:38 +0900

    refactor: export 통일

--- src/assets/webp/index.ts
+++ src/assets/webp/index.ts
@@ -1,5 +1,3 @@
-import noticeIcon from './notice-icon.webp';
-import loginNavHome from './login-nav-home.webp';
-import loginNavInfo from './login-nav-info.webp';
-
-export { noticeIcon, loginNavHome, loginNavInfo };
+export { default as noticeIcon } from './notice-icon.webp'
+export { default as loginNavHome } from './login-nav-home.webp';
+export { default as loginNavInfo } from './login-nav-info.webp';
