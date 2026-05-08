6c26e3f12baf2b30421161bd561989dc17e91ae0
eunjilee0311 <leg040311@gmail.com>
2026-05-08 13:14:39 +0900

    design: 도장 코드 모달 UI 구현

--- src/components/pages/stamp/BoothStampModal.tsx
+++ src/components/pages/stamp/BoothStampModal.tsx
@@ -48,30 +48,37 @@ export default function BoothStampModal({
   };
 
   return (
-    <div className="fixed inset-0 z-[100] flex bg-black/82">
-      <button onClick={onClose} className="text-white">
+    <div className="fixed fixed top-0 left-0 right-0 bottom-0 z-15 flex bg-black/82">
+      <button onClick={onClose} className="absolute top-21 right-4 text-white">
         X
       </button>
 
-      <section className="relative w-full flex flex-col justify-center items-center">
+      <section className="relative w-full flex flex-col justify-center items-center gap-[67px]">
         <p className="text-[24px] font-semibold text-white text-center">
           {boothId}번 부스 &lsquo;{boothName}&rsquo; <br /> 도장을 꾸욱!
         </p>
 
-        <span className="text-[16px] font-medium text-white">
-          STAFF에게 해당 화면을 보여주세요!
-        </span>
-        <input
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="코드를 입력해 주세요"
-          className="bg-white text-[16px] font-medium text-text-sub text-center px-[77px] py-[13px] rounded-[10px]"
-        ></input>
+        <div className="flex flex-col justify-center items-center gap-2 ">
+          <span className="text-[16px] font-medium text-white">
+            STAFF에게 해당 화면을 보여주세요!
+          </span>
+
+          <input
+            type="text"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="코드를 입력해 주세요"
+            className="bg-white text-[16px] font-medium text-center px-[77px] py-[13px] rounded-[10px]"
+          ></input>
+        </div>
 
         <button
           onClick={handleStampSubmit}
-          className="bg-primary px-10.5 py-3 text-[16px] font-semibold text-white rounded-[30px]"
+          className="px-10.5 py-3 text-[16px] font-semibold text-white rounded-[30px] transition-all active:scale-[0.98]"
+          style={{
+            background: "rgba(6, 56, 125, 0.50)",
+            boxShadow: "0 0 10px 0 rgba(135, 185, 255, 0.40)",
+          }}
         >
           도장 찍기
         </button>
