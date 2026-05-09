bbc594d0d7694b61274dd007a937729d4cb906e7
Jihaeee <elinkang02@gmail.com>
2026-05-09 23:33:48 +0900

    fix: 중요 공지 애니메이션 삭제 및 스타일 수정

--- src/components/pages/home/NoticeBanner.tsx
+++ src/components/pages/home/NoticeBanner.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Marquee from "@/components/ui/Marquee";
 import type { ActiveNotice } from "@/types/home";
 import { noticeIcon } from "@/assets"
 
@@ -9,12 +8,12 @@ interface Props {
 
 export function NoticeBanner({ notice }: Props) {
   return (
-    <div className="flex flex-row gap-2 whitespace-nowrap w-full p-2.5 rounded-[10px] bg-white text-black border-text-sub2 border">
+    <div className="flex flex-row gap-2 w-full p-2.5 rounded-[10px] bg-white text-black border-text-sub2 border">
       <div className="flex items-center gap-1 shrink-0">
         <Image src={noticeIcon} alt="공지" width={20} height={20} />
-        <span>중요 공지</span>
+        <span className="font-semibold">중요 공지</span>
       </div>
-      <Marquee text={notice?.title ?? ""} />
+      <span className="truncate">{notice?.title ?? ""}</span>
     </div>
   );
 }
