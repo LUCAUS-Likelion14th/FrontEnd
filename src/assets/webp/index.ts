4e34d0d85dcaffdb7acc7cf231a651ef6d4f6056
Jihaeee <elinkang02@gmail.com>
2026-05-15 23:36:22 +0900

    fix: 헤더 뜨는 문제 해결

--- src/assets/webp/index.ts
+++ src/assets/webp/index.ts
@@ -4,4 +4,5 @@ export { default as loginNavInfo } from './login-nav-info.webp';
 export { default as creditBg } from './credit-bg.webp';
 export { default as likelionLogo } from './likelion-logo.webp';
 export { default as puangBg } from './puang-bg.webp'
+export { default as headerBg } from './header-bg.webp'
 export * from './map'
