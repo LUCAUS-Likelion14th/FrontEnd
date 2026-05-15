f63b8affaee250606f523fe6c0b240e3b4923bf9
eunjilee0311 <leg040311@gmail.com>
2026-05-16 02:59:02 +0900

    design: 안내 페이지 카드 크기 조정

--- src/components/pages/info/InfoCard.tsx
+++ src/components/pages/info/InfoCard.tsx
@@ -13,7 +13,7 @@ export default function InfoCard({ imageUrl, title, link }: InfoCardProps) {
   return (
     <Link
       href={link}
-      className="group relative flex items-center justify-between w-full h-[120px] px-8 overflow-hidden rounded-[20px] active:scale-[0.98] transition-transform"
+      className="group relative flex items-center justify-between w-full h-[90px] px-8 overflow-hidden rounded-[20px] active:scale-[0.98] transition-transform"
     >
       <div className="absolute inset-0 -z-10">
         <Image
@@ -25,9 +25,7 @@ export default function InfoCard({ imageUrl, title, link }: InfoCardProps) {
         />
       </div>
 
-      <h2 className="text-white text-[22px] font-semibold z-10">
-        {title}
-      </h2>
+      <h2 className="text-white text-[18px] font-semibold z-10">{title}</h2>
     </Link>
   );
 }
