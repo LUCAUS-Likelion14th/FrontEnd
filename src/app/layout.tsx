fe72a09b53f7feb2a88d8e6897b3f1346642c1a0
Jihaeee <elinkang02@gmail.com>
2026-05-18 04:04:03 +0900

    feat: 간격 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default function RootLayout({
         )}
         <Providers>
           <Header />
-          <main className="min-h-screen mt-14" style={{ paddingTop: "env(safe-area-inset-top)", paddingBottom: "calc(1.5rem + env(safe-area-inset-bottom))" }}>
+          <main className="min-h-screen mt-14" style={{ paddingTop: "env(safe-area-inset-top)", paddingBottom: "calc(1rem + env(safe-area-inset-bottom))" }}>
             <PageTransition>{children}</PageTransition>
           </main>
           <BottomNav />
