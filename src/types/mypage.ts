688af28c3b84cf48afef5fd292392dfe9eb9503f
Jihaeee <elinkang02@gmail.com>
2026-05-17 12:33:26 +0900

    fix: api 필드명 수정

--- src/types/mypage.ts
+++ src/types/mypage.ts
@@ -44,11 +44,8 @@ export type MyBoothResponse = {
 
 export type MyBoothLikes = {
   booth_id: number;
-  location_id: number;
   booth_image: string;
   booth_name: string;
-  booth_owner: string;
-  booth_location: string;
-  like_count: number;
-  is_liked: boolean;
+  likeCount: number;
+  liked: boolean;
 };
